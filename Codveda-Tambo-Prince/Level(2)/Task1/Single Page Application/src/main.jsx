ceb2ec4ef4f9830922d0a592630e5e5be571a725
Your Name <you@example.com>
2026-09-03 15:47:58 +0200

    feat(routing): implement React Router architecture across main pages

--- Codveda-Tambo-Prince/Level(2)/Task1/Single Page Application/src/main.jsx
+++ Codveda-Tambo-Prince/Level(2)/Task1/Single Page Application/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import React from 'react'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
+  </React.StrictMode>,
 )
