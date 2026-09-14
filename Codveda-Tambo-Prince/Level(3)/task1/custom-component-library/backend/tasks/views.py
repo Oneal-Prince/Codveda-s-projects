4cefe3694ea60c72bd474b658cd6b1be89ed407f
Your Name <you@example.com>
2026-09-14 21:09:58 +0200

    feat(routing): configure baseline URL patterns to serve active landing page

--- Codveda-Tambo-Prince/Level(3)/task1/custom-component-library/backend/tasks/views.py
+++ Codveda-Tambo-Prince/Level(3)/task1/custom-component-library/backend/tasks/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+
+def home(request):
+    tasks = Task.objects.all()
+
+    context = {
+        "tasks": tasks
+    }
+
+    return render(request, "tasks/home.html", context)
